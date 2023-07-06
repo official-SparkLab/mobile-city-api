ebe176b6458d264243b95474133ce28e676bde7b
rohitpote95 <131872451+rohitpote95@users.noreply.github.com>
2023-07-06 21:40:33 +0530

    Purchase Details api Updated

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,6 @@ use App\Http\Controllers\BrandDetailsController;
 use App\Http\Controllers\ModelDetailsController;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
