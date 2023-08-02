16c650e72aa401750d3faa44b05d6c05270e6bab
Rohit <ecreativeminds20@gmail.com>
2023-08-02 17:45:59 +0530

    User Login api Update

--- app/Http/Controllers/CreateUsersController.php
+++ app/Http/Controllers/CreateUsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Create_Users;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class CreateUsersController extends Controller
 {
