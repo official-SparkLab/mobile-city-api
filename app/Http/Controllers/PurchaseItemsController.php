4d9a3bfcd70c854962b8eef10a7bb9c48758143d
rohitpote95 <131872451+rohitpote95@users.noreply.github.com>
2023-08-18 12:37:52 +0530

    Purchase Items Updated

--- app/Http/Controllers/PurchaseItemsController.php
+++ app/Http/Controllers/PurchaseItemsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Purchase_Items;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class PurchaseItemsController extends Controller
 {
     /**
